d093c931cf7d1fdb62c8dd8804c9788dc702ce38
John Jefferson Bautista <jeppbautista@gmail.com>
2019-07-18 01:34:38 +0800

    Rep 008 - Template and objects for Cart (Cart page)

--- cart.php
+++ cart.php
@@ -1,31 +1,27 @@
-
 <?php
 session_start();
 include 'class3.php';
-$class=new mydesign;
+include_once 'templates/cart.php';
+include_once 'objects/generic.php';
+include_once 'objects/product.php';
+$class = new mydesign;
 $class->database_connect();
-
 date_default_timezone_set('Asia/Manila');
-$sessiondate=date('mdY');
+$sessiondate     = date('mdY');
 $GLOBALS['test'] = 0;
 
 
-if (isset($_POST['btn-go-to-checkout'])){
-  if(count($_SESSION['cart'])>0){
-    foreach ($_SESSION['cart'] as $key => $value){
-      $_SESSION['cart'][$key]['quantity'] = $_POST['quantity-'.$key];
+if (isset($_POST['btn-go-to-checkout'])) {
+    if (count($_SESSION['cart']) > 0) {
+        foreach ($_SESSION['cart'] as $key => $value) {
+            $_SESSION['cart'][$key]['quantity'] = $_POST['quantity-' . $key];
+        }
+        echo '<script>window.location.assign("https://tbcmerchantservices.com/checkout/");</script>';
     }
-    echo '<script>window.location.assign("https://tbcmerchantservices.com/checkout/");</script>';
-  }
-  echo '<script>window.location.assign("https://tbcmerchantservices.com/shopping/");</script>';
+    echo '<script>window.location.assign("https://tbcmerchantservices.com/shopping/");</script>';
+    #TODO empty cart checkout
 }
 
-if(!isset($_SESSION['session_tbcmerchant_ctr'.$sessiondate])){
-
-}else {
-}
-
-$ctr=$_SESSION['session_tbcmerchant_ctr'.$sessiondate];
 
 $class->doc_type();
 $class->html_start('');
@@ -41,193 +37,80 @@ $class->link('https://tbcmerchantservices.com/css/style-shop.css');
 $class->script('https://tbcmerchantservices.com/js/jquery1.3.js');
 $class->link('https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 $class->head_end();
-$total = 0.0;
-$tax = 0.0;
-$shipping = 0.0;
 
-$query="select * from xtbl_account_info WHERE Main_Ctr='$ctr'";
-$rs=mysql_query($query);
-$row=mysql_fetch_assoc($rs);
-$account_type=$row['Account_Type'];
 
-if($account_type=='MERCHANT') {
-  $class->page_home_header_start();
-    $class->page_home2_header_content();
-  $class->page_home_header_end();
+if (!isset($_SESSION['session_tbcmerchant_ctr' . $sessiondate])) {
+    $class->body_start('');
+    $class->page_home_header_start();
+    $class->page_shopping_header_content1();
+    $class->page_home_header_end();
 }
-else { //if buyer
-  $class->page_home_header_start();
-    $class->page_home3_header_content();
-  $class->page_home_header_end();
+else {
+
+    $ctr = $_SESSION['session_tbcmerchant_ctr' . $sessiondate];
+    $total = 0.0;
+    $tax  = 0.0;
+    $shipping = 0.0;
+    $account_type = getAllElementsWithCondition("xtbl_account_info", "Main_Ctr", $ctr)['Account_Type'];
+
+    if ($account_type == 'MERCHANT') {
+        $class->page_home_header_start();
+        $class->page_home2_header_content();
+        $class->page_home_header_end();
+    } else { //if buyer
+        $class->page_home_header_start();
+        $class->page_home3_header_content();
+        $class->page_home_header_end();
+    }
 }
 
 include 'nav_shop.php';
-include_once 'objects/product.php';
 
-if(count($_SESSION['cart'])>0){
-  $product_ids = array();
-  foreach($_SESSION['cart'] as $id=>$value){
-    array_push($product_ids, $id);
-  }
-  $query = readByID($product_ids);
-  $rs = mysql_query($query);
-  ?>
-  <div class="" style="width:96%; margin:auto;">
-    <div class="col-12">
-      <h2 style="text-align:left">Shopping Cart</h2>
-    </div>
-    <div class="col-12 col-md-8">
-      <form class="go-to-checkout-form" method="post">
-
-      <br>
-      <table id="cart" class="table table-hover table-condensed">
-            <thead>
-            <tr>
-              <th style="width:50%">Product</th>
-              <th style="width:10%">Price</th>
-              <th style="width:8%">Quantity</th>
-              <th style="width:22%" class="text-center">Subtotal</th>
-              <th style="width:10%"></th>
-            </tr>
-          </thead>
-          <tbody>
-
-    <?php
-    while($row = mysql_fetch_assoc($rs)) {
-      $sub_total = floatval($_SESSION['cart'][$row['Ctr']]['quantity'] * $row['Product_Price']);
-      $total += $sub_total;
-      ?>
-              <tr>
-                <td data-th="Product">
-                  <div class="rowdds">
-                    <div class="col-sm-2 hidden-xs">
-                      <a href=href='<?php echo "https://tbcmerchantservices.com/item/?product=".$row['Ctr'] ?>'>
-                        <img src='<?php echo "https://tbcmerchantservices.com/products/".$row['Image'] ?>' alt="..." class="img-responsive"/>
-                      </a>
-                    </div>
-                    <div class="col-sm-10">
-                      <h4 class="nomargin"> <a href='<?php echo "https://tbcmerchantservices.com/item/?product=".$row['Ctr'] ?>'> <?php echo $row['Product_Name']; ?></a>  </h4>
-                      <p class="hidden-xs"> <?php echo mb_strimwidth($row['Product_Description'], 0, 120, "..."); ?> </p>
-                    </div>
-                  </div>
-                </td>
-                <td id='<?php echo "price-".$row["Ctr"] ?>' data-th="Price" >
-                  <span>&#8369;</span> <b><?php echo number_format($row['Product_Price'], 2); ?> </b>
-                </td>
-                <td data-th="Quantity">
-                  <input id='<?php echo "quantity-".$row["Ctr"] ?>' type="number"
-                  name='<?php echo "quantity-".$row["Ctr"] ?>' type="number"
-                  class="form-control text-center quantity-field" value='<?php echo $_SESSION['cart'][$row['Ctr']]['quantity'] ?>' min="1">
-                </td>
-                <td id='<?php echo "subtotal-".$row["Ctr"] ?>' data-th="Subtotal" class="text-center">
-                  <span>&#8369;</span><b>  <?php echo number_format($sub_total, 2) ?>  </b>
-                </td>
-                <td class="actions" data-th="">
-                  <a href=<?php echo "https://tbcmerchantservices.com/delete_from_cart.php?id=".$row["Ctr"] ?> class="btn btn-sm delete-btn" id='<?php echo "delete-".$row["Ctr"]; ?>' ><i class="fa fa-times-circle fa-2x"></i></a>
-                </td>
-              </tr>
-      <?php
+if (count($_SESSION['cart']) > 0) {
+    $product_ids = array();
+    foreach ($_SESSION['cart'] as $id => $value) {
+        array_push($product_ids, $id);
     }
-    ?>
-    </tbody>
-
-    <tfoot>
-      <!-- <tr class="visible-xs">
-        <td class="text-center"><strong> Total <span>&#8369;</span> <?php echo number_format($total,2); ?></strong></td>
-      </tr> -->
-      <tr>
-        <td><a href="#" ><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
-        <td colspan="2" class="hidden-xs"></td>
-        <td></td>
-        <!-- <td class="hidden-xs text-center"><strong>Total <span>&#8369;</span> <?php echo number_format($total,2); ?> </strong></td> -->
-        <td><button type="submit" id = "btn-go-to-checkout" name = "btn-go-to-checkout" class="btn btn-block btn-checkout">Checkout <i class="fa fa-angle-right"></i></button></td>
-      </tr>
-    </tfoot>
-    </table>
-  </form>
-
-
-    <hr>
-
-    <!-- <div class="row">
-    <div class="col-12 col-md-6">
-      <div id="discount">
-        <h5 style="text-align:left">ENTER YOUR DISCOUNT CODE</h5>
-        <div class="input-group">
-          <div class="form-group has-feedback has-clear">
-            <input type="text" class="form-control" id="discount-text" placeholder="">
-            <span class="form-control-clear glyphicon glyphicon-remove form-control-feedback hidden"></span>
-          </div>
-          <span class="input-group-btn">
-            <button type="button" class="btn" id="discount-btn" onclick="alert('Invalid discount code')">Enter</button>
-          </span>
-        </div>
-      </div>
-    </div>
-    </div> -->
-
-    </div>
-
-    <div class="col-12 col-md-4">
-    <table id="summary" class="table table-hover table-condensed">
-      <thead>
-        <tr>
-          <th colspan="2" style="width:100%"> <h4 id="summary-header">ORDER SUMMARY</h4> </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td style="width:65%"> <h5> <b>Subtotal</b> </h5> </td>
-          <td id="summ-subtotal" style="width:35%"><h5>
-            <span>&#8369;</span> <b><?php echo number_format($total, 2); ?></b>  </h5></td>
-        </tr>
-        <tr>
-          <td> <h5> <b>Tax</b> </h5> </td>
-          <td id="summ-tax">
-            <h5>
-            <span>&#8369;</span> <b><?php echo number_format($tax,2); ?></b>  </h5></td>
-        </tr>
-        <tr>
-          <td> <h5> <b>Shipping fee</b> </h5> </td>
-          <td id="summ-shipping"><h5>
-            <span>&#8369;</span> <b><?php echo number_format($shipping,2); ?></b>  </h5></td>
-        </tr>
-        <tr id="summary-total">
-          <td > <h4> <b>Total</b> </h4> </td>
-          <td id="summ-total" ><h4>
-            <span>&#8369;</span> <b><?php echo number_format($total+$tax+$shipping,2);?> </b></h4>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    </div>
-  </div>
-
-  <?php
-}
-else{
+    $query = readByID($product_ids);
+    $rs    = mysql_query($query);
+    container_start();
+    header_text("Shopping Cart");
+    table_header();
 ?>
-  <div class="container">
-    <div class="row" >
-      <div class="div-empty">
-        <img src="https://tbcmerchantservices.com/images/empty_cart.png" alt="">
-      </div>
-    </div>
-    <div class="row" style="text-align:center">
-      <b><h1> Uh Oh! Looks like your cart is <span style="color:#F11C30">empty</span>.</h1></b>
-      <br>
-      <p>You must first add items to your shopping cart before proceeding to checkout. <br>
-         You can find amazing products in the TBCMS shopping page.</p>
-    </div>
-    <div class="row">
-      <div class="div-go-back">
-        <a href="https://tbcmerchantservices.com/shopping" class="btn btn-md btn-add-to-cart">Back to shopping</a> <br>
-      </div>
-    </div>
-  </div>
+
+
 <?php
+    while ($product = mysql_fetch_assoc($rs)) {
+        $sub_total = floatval($_SESSION['cart'][$product['Ctr']]['quantity'] * $product['Product_Price']);
+        $total += $sub_total;
+        product_row($product, $sub_total);
+    }
+    table_footer();
+    order_summary($total, $tax, $shipping);
+    div_end();
 }
+else {
+  empyt_cart();
+}
+
 $class->page_welcome_header_content_start_footer();
 $class->body_end();
 $class->html_end();
- ?>
+?>
+
+<!-- <div class="row">
+<div class="col-12 col-md-6">
+<div id="discount">
+<h5 style="text-align:left">ENTER YOUR DISCOUNT CODE</h5>
+<div class="input-group">
+<div class="form-group has-feedback has-clear">
+<input type="text" class="form-control" id="discount-text" placeholder="">
+<span class="form-control-clear glyphicon glyphicon-remove form-control-feedback hidden"></span>
+</div>
+<span class="input-group-btn">
+<button type="button" class="btn" id="discount-btn" onclick="alert('Invalid discount code')">Enter</button>
+</span>
+</div>
+</div>
+</div>
+</div> -->
---
+++ templates/cart.php
@@ -0,0 +1,236 @@
+<?php
+  function container_start(){
+    ?>
+    <div class="" style="width:96%; margin:auto;">
+    <?php
+  }
+
+  function header_text($text){
+    ?>
+    <div class="col-12">
+      <h2 style="text-align:left"> <?php echo $text ?>
+      </h2>
+    </div>
+    <?php
+  }
+
+  function table_header(){
+    ?>
+    <div class="col-12 col-md-8">
+      <form class="go-to-checkout-form" method="post">
+        <br>
+        <table id="cart" class="table table-hover table-condensed">
+          <thead>
+            <tr>
+              <th style="width:50%">Product </th>
+              <th style="width:10%">Price </th>
+              <th style="width:8%">Quantity </th>
+              <th style="width:22%" class="text-center">Subtotal </th>
+              <th style="width:10%"> </th>
+            </tr>
+          </thead>
+          <tbody>
+    <?php
+  }
+
+  function product_row($product, $sub_total){
+    $id = $product['Ctr'];
+    $quantity = $_SESSION['cart'][$product['Ctr']]['quantity'];
+    $image = $product['Image'];
+    $product_name = $product['Product_Name'];
+    $product_desc = $product['Product_Description'];
+    $product_price = $product['Product_Price'];
+    ?>
+
+    <tr>
+       <td data-th="Product">
+         <div class="rowdds">
+           <div class="col-sm-2 hidden-xs">
+             <a href=href='<?php echo "https://tbcmerchantservices.com/item/?product=" . $id;?>'>
+               <img src='<?php echo "https://tbcmerchantservices.com/products/" . $image;?>' alt="..." class="img-responsive"/>
+             </a>
+           </div>
+           <div class="col-sm-10">
+             <h4 class="nomargin">
+               <a href='<?php echo "https://tbcmerchantservices.com/item/?product=" . $id;?>'>
+                 <?php echo $product_name;?> </a>
+             </h4>
+             <p class="hidden-xs">
+               <?php echo mb_strimwidth($product_desc, 0, 120, "...");?>
+             </p>
+           </div>
+         </div>
+       </td>
+       <td id='<?php echo "price-" . $id; ?>' data-th="Price" >
+         <span>&#8369;</span>
+         <b><?php echo number_format($product_price, 2);?></b>
+       </td>
+       <td data-th="Quantity">
+         <input id='<?php echo "quantity-" . $id; ?>' type="number"
+                name='<?php echo "quantity-" . $id; ?>' type="number"
+                class="form-control text-center quantity-field" value='<?php echo $quantity;?>'
+                min="1">
+       </td>
+       <td id='<?php echo "subtotal-" . $id; ?>' data-th="Subtotal" class="text-center">
+         <span>&#8369; </span>
+         <b> <?php echo number_format($sub_total, 2); ?> </b>
+       </td>
+       <td class="actions" data-th="">
+         <a href= <?php echo "https://tbcmerchantservices.com/delete_from_cart.php?id=" . $id; ?>
+           class="btn btn-sm delete-btn" id='<?php echo "delete-" . $id;?>' >
+           <i class="fa fa-times-circle fa-2x"> </i>
+         </a>
+     </td>
+   </tr>
+
+    <?php
+  }
+
+  function table_footer(){
+    ?>
+      </tbody>
+      <tfoot>
+       <tr>
+         <td>
+           <a href="https://tbcmerchantservices/shopping" >
+             <i class="fa fa-angle-left"></i> Continue Shopping
+           </a>
+         </td>
+         <td colspan="2" class="hidden-xs"> </td>
+         <td>
+           <button type="submit" id = "btn-go-to-checkout" name = "btn-go-to-checkout" class="btn btn-block btn-checkout">Checkout
+             <i class="fa fa-angle-right"> </i>
+           </button>
+         </td>
+       </tr>
+     </tfoot>
+     </table>
+    </form>
+    <hr>
+  </div>
+    <?php
+  }
+
+  function order_summary($total, $tax, $shipping){
+    ?>
+    <div class="col-12 col-md-4">
+      <table id="summary" class="table table-hover table-condensed">
+        <thead>
+          <tr>
+            <th colspan="2" style="width:100%">
+              <h4 id="summary-header">ORDER SUMMARY
+              </h4>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style="width:65%">
+              <h5>
+                Subtotal
+              </h5>
+            </td>
+            <td id="summ-subtotal" style="width:35%">
+              <h5>
+                <span>&#8369;
+                </span>
+                <b>
+                  <?php echo number_format($total, 2);?>
+               </b>
+              </h5>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h5>
+                <b>Tax
+                </b>
+              </h5>
+            </td>
+            <td id="summ-tax">
+              <h5>
+                <span>&#8369;
+                </span>
+                <b>
+                  <?php echo number_format($tax, 2);?>
+               </b>
+              </h5>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h5>
+                Shipping fee
+              </h5>
+            </td>
+            <td id="summ-shipping">
+              <h5>
+                <span>&#8369;
+                </span>
+                <b>
+                  <?php echo number_format($shipping, 2);?>
+               </b>
+              </h5>
+            </td>
+          </tr>
+          <tr id="summary-total">
+            <td >
+              <h4>
+                Total
+
+              </h4>
+            </td>
+            <td id="summ-total" >
+              <h4>
+                <span>&#8369;
+                </span>
+                <b>
+                  <?php echo number_format($total + $tax + $shipping, 2);?>
+                </b>
+              </h4>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <?php
+  }
+
+  function div_end(){
+    ?>
+    </div>
+    <?php
+  }
+
+  function empty_cart(){
+    ?>
+    <div class="container">
+      <div class="row" >
+        <div class="div-empty">
+          <img src="https://tbcmerchantservices.com/images/empty_cart.png" alt="">
+        </div>
+      </div>
+      <div class="row" style="text-align:center">
+        <b>
+          <h1> Uh Oh! Looks like your cart is
+            <span style="color:#F11C30">empty
+            </span>.
+          </h1>
+        </b>
+        <br>
+        <p>You must first add items to your shopping cart before proceeding to checkout.
+          <br>
+          You can find amazing products in the TBCMS shopping page.
+        </p>
+      </div>
+      <div class="row">
+        <div class="div-go-back">
+          <a href="https://tbcmerchantservices.com/shopping" class="btn btn-md btn-add-to-cart">Back to shopping
+          </a>
+          <br>
+        </div>
+      </div>
+    </div>
+    <?php
+  }
+?>
